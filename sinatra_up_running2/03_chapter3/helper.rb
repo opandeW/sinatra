817579c0aec92341f479cb13d7cd76d2e2089971
opandeW <wopande@gmail.com>
2016-10-19 23:10:06 +0300

    a helper method that generates hyperlinks

---
+++ sinatra_up_running2/03_chapter3/helper.rb
@@ -0,0 +1,14 @@
+require 'sinatra/base'
+
+module Sinatra
+	module LinkHelper
+		def link(name)
+			case name
+			when :about then '/about'
+			when :index then '/index'
+			else "/page/#{name}"
+		end
+	end
+
+	helpers LinkHelper
+end
